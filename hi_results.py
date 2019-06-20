6eaddbc0af6dc80e2271d36355b041e757b605ff
Cathy Egboh <cEgboh@lienteam.com>
2019-06-20 11:39:49 -0500

    Updated the code to use groupby function to improve the speed

--- hi_results.py
+++ hi_results.py
@@ -17,22 +17,74 @@ def col_3(path):
     combined_df = pd.read_excel(r'./excel_results/Full_Analysis.xlsx')
 
     # seperate each label into its own df
-    hi_cm = combined_df[combined_df['LF_Label'] == 'Human Intervention (CM)']
-    hi_level1 = combined_df[combined_df['LF_Label'] == 'Human Intervention (fix this week)']
-    hi_level2 = combined_df[combined_df['LF_Label'] == 'Human Intervention (fix this week if time)']
-    hi_level3 = combined_df[combined_df['LF_Label'] == 'Human Intervention (fix when you can)']
-    hi_level4 = combined_df[combined_df['LF_Label'] == 'Human Intervention - Close in SLAM']
-    no_change = combined_df[combined_df['LF_Label'] == 'No Changes, No Issues']
-    look_into = combined_df[combined_df['LF_Label'] == 'Look Into']
-    add_lien = combined_df[combined_df['LF_Label'] == 'Add Lien']
+    df_hi = combined_df.groupby('LF_Label')
+    try:
+        hi_cm = df_hi.get_group('Human Intervention (CM)')
+    except KeyError:
+        pass
+    try:
+        hi_level1 = df_hi.get_group('Human Intervention (fix this week)')
+    except KeyError:
+        pass
+    try:
+        hi_level2 = df_hi.get_group('Human Intervention (fix this week if time)')
+    except KeyError:
+        pass
+    try:
+        hi_level3 = df_hi.get_group('Human Intervention (fix when you can)')
+    except KeyError:
+        pass
+    try:
+        hi_level4 = df_hi.get_group('Human Intervention - Close in SLAM')
+    except KeyError:
+        pass
+    try:
+        no_change = df_hi.get_group('No Changes, No Issues')
+    except KeyError:
+        pass
+    try:
+        look_into = df_hi.get_group('Look Into')
+    except KeyError:
+        pass
+    try:
+        add_lien = df_hi.get_group('Add Lien')
+    except KeyError:
+        pass
+ 
 
-    # export into worksheet
-    ud.export(hi_cm, "Human_Intervention_CM - ", path)
-    ud.export(hi_level1, "Human_Intervention_L1 - ", path)
-    ud.export(hi_level2, "Human_Intervention_L2 - ", path)
-    ud.export(hi_level3, "Human_Intervention_L3 - ", path)
-    ud.export(hi_level4, "Human_Intervention_L4 - ", path)
-    ud.export(no_change, "No_Change - ", path)
-    ud.export(look_into, "Check Query - ", path)
-    ud.export(add_lien, "Add Liens - ", path)
+    # export into worksheet and put a filename into it
+    try:
+        ud.export(hi_cm, "Human_Intervention_CM - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(hi_level1, "Human_Intervention_L1 - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(hi_level2, "Human_Intervention_L2 - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(hi_level3, "Human_Intervention_L3 - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(hi_level4, "Human_Intervention_L4 - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(no_change, "No_Change - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(look_into, "Check Query - ", path)
+    except UnboundLocalError:
+        pass
+    try:
+        ud.export(add_lien, "Add Liens - ", path)
+    except UnboundLocalError:
+        pass
+    
     print('Human Intervention is done, the script is done.')
+    
