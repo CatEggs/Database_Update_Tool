c1507a2e35c344d968d6f8e99e3748767f1f30dd
CatEggs <egboh.catherine@utexas.edu>
2019-12-04 14:21:34 -0600

    Removed any unnecessary libraries

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import analysis
 import update
 import hi_results
-import updatelib as ud
 import time
 import concurrent.futures
 from os import listdir
 
-
-start = time.perf_counter()
-
-
-def main():
+#user_input = "2019-11-18" #input('Tell me the folder date (yyyy_mm_dd):')
+def main(user_input):
+    start = time.perf_counter()
     # define the file path
-    hi_path = r'F:\Mass Tort Cases\TVM\Claims Online\Updates\2019\2019_11_04\Human_Intervention\\'
-    path = r'F:\Mass Tort Cases\TVM\Claims Online\Updates\2019\2019_11_04\OBE\\'
+    hi_path = r'F:\Mass Tort Cases\TVM\Claims Online\Updates\2019\\'+ user_input +'\Human_Intervention\\'
+    path = r'F:\Mass Tort Cases\TVM\Claims Online\Updates\2019\\'+ user_input +'\TBU\\'
 
     # put .xlsx files found in path to list
     file_list = []
     for f in listdir(path):
         if f.endswith('.xlsx'):
             file_list.append(f)
+
     # run SQL analysis
     analysis.col_1()
     print('Analysis is done, now for human intervention results!')
@@ -28,12 +26,15 @@ def main():
     hi_results.col_3(hi_path)
 
     # run update script to update the OBE's with info from analysis
+    print(file_list)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for f in file_list:
             executor.submit(update.col_2, path, f)
 
-if __name__ == '__main__':
-    main()
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish-start, 2)} seconds')
 
-finish = time.perf_counter()
-print(f'Finished in {round(finish-start, 2)} seconds')
+if __name__ == '__main__':
+    user_input = input('Tell me the folder date (yyyy-mm-dd):')
+    main(user_input)
+#main(user_input)
